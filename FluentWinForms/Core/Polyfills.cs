670f8812103c2958ecaa2947c88a0d780eb72422
Diegoxd12wee436 <pirataxd59@gmail.com>
2026-07-10 20:48:06 -0600

    Refactor:Polyfills deleted because init is available since net 5

--- FluentWinForms/Core/Polyfills.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel;
-
-// Esto le enseña al .NET 4.8 cómo usar el 'init' moderno
-namespace System.Runtime.CompilerServices
-{
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    internal static class IsExternalInit { }
-}
